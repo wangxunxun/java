a79297fc404afb9ec24a9ce2b96318dcf3d2d847
wangxun <troy.wang@beyondsoft.com>
2015-10-27 16:22:04 +0800

    111

--- appautotest/src/test/java/NPAndroid/ad.java
+++ appautotest/src/test/java/NPAndroid/ad.java
@@ -37,24 +37,17 @@ public class ad {
     	NPAndroid.waitDisplay("启动页", "略过");
     	NPAndroid.clickElement("启动页", "略过");
     	NPAndroid.sleep(20000);
-/*    	NPAndroid.swipeOfType("up");
-    	NPAndroid.swipeOfType("up");*/
-//    	NPAndroid.sleep(5000);
-    	NPAndroid.waitDisplay("首页","文章");
-//    	NPAndroid.clickElement("首页","文章");
+    	NPAndroid.swipeOfType("up");
+    	NPAndroid.swipeOfType("up");
+    	NPAndroid.swipeOfType("up");
     	NPAndroid.sleep(5000);
-    	NPAndroid.clickElementByXY("首页","文章");
-//    	NPAndroid.tab(60, 100);
-//    	NPAndroid.swipeOfType("left");
-/*    	NPAndroid.sleep(15000);
-    	NPAndroid.waitDisplay("首页","文章");
-    	NPAndroid.getImageViews();
-    	
-    	
-    	NPAndroid.log(NPAndroid.getElementX("首页","文章"));
-    	NPAndroid.log(NPAndroid.getElementY("首页","文章"));
-    	NPAndroid.log(NPAndroid.getElementLocateX("首页","文章"));
-    	NPAndroid.log(NPAndroid.getElementLocateY("首页","文章"));*/
+    	NPAndroid.waitDisplay("首页","广告");
+    	NPAndroid.sleep(5000);
+    	int x = NPAndroid.getElementX("首页","广告");
+    	int y = NPAndroid.getElementY("首页","广告");
+    	NPAndroid.log(x);
+    	NPAndroid.log(y);
+
 
     	
 
